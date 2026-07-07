968b09359f9b135fc80cff5ebb2bb22c7ff07bce
CapriBlu <pawel.petruk@hotmail.com>
2026-07-07 15:31:15 +0200

    Pubblica anche cucina pizzeria al mensile

--- publish-monthly.js
+++ publish-monthly.js
@@ -7,14 +7,24 @@ function publishWeekToMonthly() {
   const weekValue = weekInput.value;
   const sourceKey = "capriBluTurniStaffWeekV1-" + weekValue;
   const publishedKey = "capriBluTurniStaffPublishedWeekV1-" + weekValue;
+  const kitchenSourceKey = "capriBluTurniCucinaWeekV1-" + weekValue;
+  const kitchenPublishedKey = "capriBluTurniCucinaPublishedWeekV1-" + weekValue;
   const currentData = localStorage.getItem(sourceKey) || localStorage.getItem("capriBluTurniStaffV5");
+  const kitchenData = localStorage.getItem(kitchenSourceKey);
 
-  if (!currentData) {
+  if (!currentData && !kitchenData) {
     alert("Non ci sono turni da inviare per questa settimana.");
     return;
   }
 
-  localStorage.setItem(publishedKey, currentData);
+  if (currentData) {
+    localStorage.setItem(publishedKey, currentData);
+  }
+
+  if (kitchenData) {
+    localStorage.setItem(kitchenPublishedKey, kitchenData);
+  }
+
   localStorage.setItem("capriBluUltimaSettimanaInviata", weekValue);
   alert("Turni inviati al mensile per la settimana " + weekValue + ".");
 }
